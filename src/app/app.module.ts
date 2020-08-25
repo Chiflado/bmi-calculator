4ad648a1165601320b6e41ea4387c6fef160736e
Chiflado <prevarabalazs@gmail.com>
2020-08-25 13:25:04 +0200

    changes colors and adds bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SideNavbarComponent } from './layout/side-navbar/side-navbar.component';
+import { TopNavbarComponent } from './layout/top-navbar/top-navbar.component';
+import { PersonalDataComponent } from './pages/personal-data/personal-data.component';
+import { MeasurementsComponent } from './pages/measurements/measurements.component';
+import { SummaryComponent } from './pages/summary/summary.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    SideNavbarComponent,
+    TopNavbarComponent,
+    PersonalDataComponent,
+    MeasurementsComponent,
+    SummaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
